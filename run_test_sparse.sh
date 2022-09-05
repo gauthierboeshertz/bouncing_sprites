dd0eb36f1432db7c27d5031a2990c1ed4629d2dc
Gauthier Boeshertz <gauthierboeshertz@student-net-vpn-1554.intern.ethz.ch>
2022-09-04 23:55:37 -0400

    ok

--- run_test_sparse.sh
+++ run_test_sparse.sh
@@ -1,34 +1,34 @@
-python3 gym_example.py  --num_sprites 1 --sparse_rewards
-python3 gym_example.py  --num_sprites 2 --sparse_rewards
-python3 gym_example.py  --num_sprites 3 --sparse_rewards
-python3 gym_example.py  --num_sprites 4 --sparse_rewards
+python3 gym_example.py  --num_sprites 1 --sparse_reward
+python3 gym_example.py  --num_sprites 2 --sparse_reward
+python3 gym_example.py  --num_sprites 3 --sparse_reward
+python3 gym_example.py  --num_sprites 4 --sparse_reward
 
 
-python3 gym_example.py  --one_sprite_mover  --num_sprites 1 --sparse_rewards
-python3 gym_example.py  --one_sprite_mover  --num_sprites 2 --sparse_rewards
-python3 gym_example.py  --one_sprite_mover  --num_sprites 3 --sparse_rewards
-python3 gym_example.py  --one_sprite_mover  --num_sprites 4 --sparse_rewards
+python3 gym_example.py  --one_sprite_mover  --num_sprites 1 --sparse_reward
+python3 gym_example.py  --one_sprite_mover  --num_sprites 2 --sparse_reward
+python3 gym_example.py  --one_sprite_mover  --num_sprites 3 --sparse_reward
+python3 gym_example.py  --one_sprite_mover  --num_sprites 4 --sparse_reward
 
 
 
-python3 gym_example.py  --all_sprite_mover  --num_sprites 1 --sparse_rewards
-python3 gym_example.py  --all_sprite_mover  --num_sprites 2 --sparse_rewards
-python3 gym_example.py  --all_sprite_mover  --num_sprites 3 --sparse_rewards
-python3 gym_example.py  --all_sprite_mover  --num_sprites 4 --sparse_rewards
+python3 gym_example.py  --all_sprite_mover  --num_sprites 1 --sparse_reward
+python3 gym_example.py  --all_sprite_mover  --num_sprites 2 --sparse_reward
+python3 gym_example.py  --all_sprite_mover  --num_sprites 3 --sparse_reward
+python3 gym_example.py  --all_sprite_mover  --num_sprites 4 --sparse_reward
 
 
-python3 gym_example.py  --num_sprites 1 --sparse_rewards --random_init_places
-python3 gym_example.py  --num_sprites 2 --sparse_rewards --random_init_places
-python3 gym_example.py  --num_sprites 3 --sparse_rewards --random_init_places
-python3 gym_example.py  --num_sprites 4 --sparse_rewards --random_init_places
+python3 gym_example.py  --num_sprites 1 --sparse_reward --random_init_places
+python3 gym_example.py  --num_sprites 2 --sparse_reward --random_init_places
+python3 gym_example.py  --num_sprites 3 --sparse_reward --random_init_places
+python3 gym_example.py  --num_sprites 4 --sparse_reward --random_init_places
 
 
-python3 gym_example.py  --one_sprite_mover  --num_sprites 1 --sparse_rewards --random_init_places
-python3 gym_example.py  --one_sprite_mover  --num_sprites 2 --sparse_rewards --random_init_places
-python3 gym_example.py  --one_sprite_mover  --num_sprites 3 --sparse_rewards --random_init_places
-python3 gym_example.py  --one_sprite_mover  --num_sprites 4 --sparse_rewards --random_init_places
+python3 gym_example.py  --one_sprite_mover  --num_sprites 1 --sparse_reward --random_init_places
+python3 gym_example.py  --one_sprite_mover  --num_sprites 2 --sparse_reward --random_init_places
+python3 gym_example.py  --one_sprite_mover  --num_sprites 3 --sparse_reward --random_init_places
+python3 gym_example.py  --one_sprite_mover  --num_sprites 4 --sparse_reward --random_init_places
 
-python3 gym_example.py  --all_sprite_mover  --num_sprites 1 --sparse_rewards --random_init_places
-python3 gym_example.py  --all_sprite_mover  --num_sprites 2 --sparse_rewards --random_init_places
-python3 gym_example.py  --all_sprite_mover  --num_sprites 3 --sparse_rewards --random_init_places
-python3 gym_example.py  --all_sprite_mover  --num_sprites 4 --sparse_rewards --random_init_places
+python3 gym_example.py  --all_sprite_mover  --num_sprites 1 --sparse_reward --random_init_places
+python3 gym_example.py  --all_sprite_mover  --num_sprites 2 --sparse_reward --random_init_places
+python3 gym_example.py  --all_sprite_mover  --num_sprites 3 --sparse_reward --random_init_places
+python3 gym_example.py  --all_sprite_mover  --num_sprites 4 --sparse_reward --random_init_places
